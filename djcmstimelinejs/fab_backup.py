37fe199a98dfb9e26bcee74b6d34b8fb1cf1b8d1
Dmitry Derevyankin <derdmitry@gmail.com>
2014-05-16 19:24:53 +0300

    fabric paralles

--- djcmstimelinejs/fab_backup.py
+++ djcmstimelinejs/fab_backup.py
@@ -10,8 +10,9 @@ __all__ = ['backup_db', 'backup_files']
 
 def mysqldump(db):
     date = time.strftime('%Y%m%d')
-    dump_file = '/tmp/%(database)s-%(date)s.sql' % {'database': db['name'],
-                                                    'date': date}
+    dump_file = '/tmp/%(database)s-%(host)s-%(date)s.sql' % {'database': db['name'],
+                                                             'host':db['host'],
+                                                            'date': date}
     db['dump_file'] = dump_file
 
     local('mysqldump --no-create-db --opt '
@@ -65,44 +66,52 @@ def mount_remote_store_with_pass():
     return mount_point
 
 def mount_remote_store():
-    mount_point = env.backup_remote_store['mount_point']
-    if not os.path.exists(mount_point):
-        local('mkdir -p %s' % mount_point)
+    mount_points = []
+    for backup_remote_store in env.backup_remote_store:
+        mount_point = "%s-%s" % (backup_remote_store['host'],
+                                 backup_remote_store['mount_point'])
 
-    local("echo '%(password)s' | "
-          "sshfs -o port=%(port)s %(user)s@%(host)s:/%(path)s %(mount_point)s" % env.backup_remote_store)
+        if not os.path.exists(mount_point):
+            local('mkdir -p %s' % mount_point)
+        backup_remote_store['mount_point'] = mount_point
+        local("echo '%(password)s' | "
+              "sshfs -o port=%(port)s %(user)s@%(host)s:/%(path)s %(mount_point)s" % backup_remote_store)
 
-    return mount_point
+        mount_points.append(mount_point)
+
+    return mount_points
 
 def umount_remote_store():
-    local("fusermount -u %s" % env.backup_remote_store['mount_point'])
+    for backup_remote_store in env.backup_remote_store:
+        local("fusermount -u %s" % backup_remote_store['mount_point'])
 
 
 def copy_dumps_to_remote_store(dump_file_list):
-    remote_store_path = mount_remote_store()
-    date = time.strftime('%Y%m%d')
-    remote_dump_path = os.path.join(remote_store_path,
-                                    env.backup_remote_store['sub_path_db'],
-                                    date)
 
-    if not os.path.exists(remote_dump_path):
-        local('mkdir -p %s' % remote_dump_path)
+    for i, remote_store_path in enumerate(mount_remote_store()):
+        date = time.strftime('%Y%m%d')
+        remote_dump_path = os.path.join(remote_store_path,
+                                        env.backup_remote_store[i]['sub_path_db'],
+                                        date)
 
-    for dump_file in dump_file_list:
-        basename_dump_file = os.path.basename(dump_file)
-        remote_dump_file = os.path.join(remote_dump_path, basename_dump_file)
-        local("cp %s %s" % (dump_file, remote_dump_file))
-        # for local_path in env.backup_files_list:
-        #     local_dump_file = os.path.join(local_path['localpath'], basename_dump_file)
-        #     local("cp %s %s" % (remote_dump_file, local_dump_file))
+        if not os.path.exists(remote_dump_path):
+            local('mkdir -p %s' % remote_dump_path)
 
-    print "copy dump: cp %s %s" % (dump_file, remote_dump_file)
+        for dump_file in dump_file_list:
+            basename_dump_file = os.path.basename(dump_file)
+            remote_dump_file = os.path.join(remote_dump_path, basename_dump_file)
+            local("cp %s %s" % (dump_file, remote_dump_file))
+            for local_path in env.backup_files_list:
+                local_dump_file = os.path.join(local_path['localpath'], basename_dump_file)
+                local("cp %s %s" % (remote_dump_file, local_dump_file))
+
+        print "copy dump: cp %s %s" % (dump_file, remote_dump_file)
     umount_remote_store()
 
-
+@parallel
 def backup_db():
     dump_file_list = make_local_dumps()
-    print dump_file_list
+    print "Files list: \n", dump_file_list
     copy_dumps_to_remote_store(dump_file_list)
 
 @parallel
